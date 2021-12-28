d408603dee103b3009e4c68ebe3ad5b34532efa4
dollardhingra <dollar.dhingra@kyruus.com>
2021-12-28 19:44:23 +0530

    added user model and methods

---
+++ app/core/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        create and save a new user
+        :param email:
+        :param password:
+        :param extra_fields:
+        :return:
+        """
+        if not email:
+            raise ValueError("Users must have email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        create and save a new superuser
+        :param email:
+        :param password:
+        :return:
+        """
+        if not email:
+            raise ValueError("Users must have email address.")
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    custom user model, that supports email instead of username
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
